3f986438111d5d0fd36c16c7745d70e572ea6206
Alex <alexmaly@i.ua>
2018-06-04 01:49:50 +0300

    next step with double array

--- main.cpp
+++ main.cpp
@@ -10,24 +10,21 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <stdio.h>
 
 //#include "chess.ini"
 //#include "chess.h"
 
 using namespace std;
-/////////////////////////START
-/////////////////////////START
-/////////////////////////START
 
 enum {NOTHING = 0, PAWN = 1, BISHOP = 2, KNIGHT = 3, ROOK = 4, QUEEN = 5, KING = 6, CHECKER = 10};//figure_status 
 enum {DEAD = 0, ALIVE};//figure_state 
-enum {BLACK = 0, WHITE};//figure_membership
-// two enums for classical fefinitions
-enum {ONE = 0, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT}; // vertical
+enum {BLACK = 1, WHITE};//figure_membership
+enum {ONE = 0, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, BOARD_SIDE}; // vertical
 enum {A = 0, B, C, D, E, F, G, H}; // horizontal
-const int board_side = 8; 
+ 
 
-class coords
+class coordinates
 {
 public:
 	int x_coord;
@@ -37,13 +34,13 @@ public:
 
 class Figure
 {
-public:
-	int x_coord;
-	int y_coord;
-	int status;
-	bool membership;
-	bool state;
 //public:
+	int x_coord;
+	int y_coord; 
+	int status;
+	int membership;
+	bool state;
+public:
 	Figure()
 	{
 		x_coord = A;
@@ -52,7 +49,8 @@ public:
 		membership = BLACK;
 		state = ALIVE;
 	}
-	Figure(int horizontal, int vertical, int _status, bool _membership, bool _state)
+
+	Figure(int horizontal, int vertical, int _status, int _membership, bool _state)
 	{
 		x_coord = horizontal;
 		y_coord = vertical;
@@ -76,6 +74,11 @@ public:
 		*/
 
 	//}
+	int get_horizontal() {	return x_coord;	}
+	int get_vertical() {	return y_coord;	}
+	int get_status() {	return status;	}
+	int get_membership() {	return membership;	}
+	int get_state() {	return state;	}
 	
 };
 
@@ -90,9 +93,8 @@ public:
 	~Player();
 	
 };
-
-void move(Figure *Current_figure)
-//void move(int status, int _x, int _y) // class affiliation is undefined
+/*
+void move(Figure *Current_figure)// class affiliation is undefined
 { 
 	int x = Current_figure->x_coord;
 	int y = Current_figure->y_coord;
@@ -105,7 +107,7 @@ void move(Figure *Current_figure)
 	//		1.1) its status ( deal done == "y + 1" and so on)
 	//		AND
 	//		1.2) its current position (are there these coords in limits of the board or not)
-	//		( deal done == ">= board_side" and so on)
+	//		( deal done == ">= BOARD_SIDE" and so on)
 	switch(status)
 	{
 		case CHECKER: 
@@ -129,8 +131,8 @@ void move(Figure *Current_figure)
 				if(	// board overflow cases
 					current_coords[i].y_coord < 0 ||
 					current_coords[i].x_coord < 0 ||
-					current_coords[i].y_coord >= board_side ||
-					current_coords[i].x_coord >= board_side
+					current_coords[i].y_coord >= BOARD_SIDE ||
+					current_coords[i].x_coord >= BOARD_SIDE
 				) { current_coords[i].y_coord = 100; current_coords[i].x_coord = 100; }
 			}
 
@@ -143,31 +145,100 @@ void move(Figure *Current_figure)
 		default:
 			cout << " incorrect argument (figure status)" << endl;
 	}
-	/*	- as a result should be known the finite range of possibile new position(s)
-		2) is there other figure there
-		2.1) is it a stranger or a own
-		3) will the new position be able to injure 
-	*/
+	//	- as a result should be known the finite range of possibile new position(s)
+	//	2) is there other figure there
+	//	2.1) is it a stranger or a own
+	//	3) will the new position be able to injure 
+	
 	return ;	
-}
+}*/
 
-/////////////////////////FINISH
-/////////////////////////FINISH
-/////////////////////////FINISH
-
-Figure *Fig[board_side] [board_side];
+Figure *Fig[BOARD_SIDE][BOARD_SIDE] = {nullptr};
+void set_All_Figures(Figure *, int );
 
 int main(void)
 {
-	
-	
-		Fig[i][] = new Figure(horizontal, vertical, _status, _membership, _state);
-	
-	
+//	set_All_Figures(Fig, BOARD_SIDE);
+	int horizontal = A, vertical = ONE, _status = CHECKER, _membership = 0; 
+	bool _state = 0;
+	for (int h = 0; h < BOARD_SIDE; ++h)
+	{
+		for (int v = 0; v < BOARD_SIDE; ++v) 
+		{
+			if ( 
+				( v == ONE && (!(h%2)) ) ||
+				( v == TWO && ((h%2)) ) ||
+				( v == THREE && (!(h%2)) ) ||
+				( v == SIX && ((h%2)) ) ||
+				( v == SEVEN && (!(h%2)) ) ||
+				( v == EIGHT && ((h%2)) )
+			)
+			{
+				_state = ALIVE;
+				if ( v == ONE ||
+					 v == TWO ||
+					 v == THREE )
+					 _membership = BLACK;
+		
+				if ( v == SIX ||
+					 v == SEVEN ||
+					 v == EIGHT )
+					 _membership = WHITE;
+			}
+			else
+			{
+				_state = DEAD;
+				_membership = NOTHING;
+			}
+			horizontal = h + 1;
+			vertical = v + 1;
+			//horizontal = h; it is a case without ZERO
+			//vertical = v;	it is a case without ZERO
+			
+			Fig[v][h] = new Figure(horizontal, vertical, _status, _membership, _state);	
+		}		
+	}
 
-	move(Fig[0]);
+	cout << "\n" << endl;
+	for (int v = (BOARD_SIDE - 1); v >= 0; --v)
+	{
+		for (int h = 0; h < BOARD_SIDE; ++h) 
+		{
+			cout << " [" << Fig[v][h]->get_state() << " " << Fig[v][h]->get_membership() <<"] ";
+		}
+		cout << "\n" << endl;
+	}
 
-	delete Fig[board_side * board_side];
+	for (int h = 0; h < BOARD_SIDE; ++h)
+		for (int v = 0; v < BOARD_SIDE; ++v) 
+			delete Fig[v][h];
+	
+	// HOW TO CHECK MEMORY CLEANING ?
+	// HOW WRITE CORRECT Pointer Figure[][] as an argument ?
+	// HOW to replace/substitute part of code without aguments and a returned value ?
+	// how to use struct/class "coords" within class Figure 
+
+	//move(Fig[0][0]);
+
+	
 	
 	return 0;
 }
+/*
+void set_All_Figures(Figure *Fig, int BOARD_SIDE)
+{
+	int horizontal = A, vertical = ONE, _status = CHECKER; 
+	bool _membership = 0, _state = 0;
+	for (int h = 0; h < BOARD_SIDE; ++h)
+	{
+		for (int v = 0; v < BOARD_SIDE; ++v) 
+		{
+			if(h == ONE)
+				Fig[v][h] = new Figure(horizontal, vertical, _status, _membership, 1);
+			else
+				Fig[v][h] = new Figure(horizontal, vertical, _status, _membership, 0);
+		}
+		
+	}
+}
+*/
